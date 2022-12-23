ff8373e97ee28c1623d91e57692a75e0c96f10e9
josephlarrivy <joseph.larrivy@gmail.com>
2022-12-23 10:30:46 -0600

    now able to add/view industries

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const app = express();
 
 const companyRoutes = require("./routes/companies");
 const invoiceRoutes = require("./routes/invoices");
+const industriesRoutes = require("./routes/industries")
 
 const ExpressError = require("./expressError")
 
 app.use(express.json());
 app.use("/companies", companyRoutes);
 app.use("/invoices", invoiceRoutes);
+app.use("/industries", industriesRoutes)
 
 
 
